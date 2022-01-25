c735c36dbe9a88c0047a812c6607e2593fd2c81c
Christian Monrad Mortensen <chri615w@edu.sde.dk>
2022-01-25 12:23:12 +0100

    hajsdhasj

---
+++ Rat_race/Rat_race/Bookmaker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rat_race
+{
+    internal class Bookmaker
+    {
+        public List<Bet> Bets { get; set; }
+
+        // TK
+        public Bet PlaceBet(Race race, Rat rat, Player player, int money)
+        {
+            return new Bet(money,player,race,rat);
+        }
+
+    }
+}
