62503acec672457acdb76970b43eb3408ec5f9d6
vanzeku <thom196c@edu.sde.dk>
2022-01-25 11:31:22 +0100

    rat and random

---
+++ Rat_race/Rat_race/Rat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rat_race
+{
+    public class Rat
+    {
+        public string Name;
+        private int _position;
+        public int Position { get; set; }
+
+        public void ResetRat()
+        {
+            Position = 0;
+        }
+        public int MoveRat()
+        {
+            _position = RNG.Range(0, 5);
+            Position += _position;
+        }
+    }
+}
